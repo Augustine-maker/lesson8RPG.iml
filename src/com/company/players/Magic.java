d5bcea76daae121b34ac1ed58a2a2747591bda95
Augustine-maker <alaimirjigit@mail.ru>
2021-09-03 23:05:41 +0600

    Initial commit

---
+++ src/com/company/players/Magic.java
@@ -0,0 +1,13 @@
+package com.company.players;
+
+public class Magic extends Hero {
+
+    public Magic(int health, int damage) {
+        super(health, damage, SuperAbility.BOOST);
+    }
+
+    @Override
+    public void applySuperAbility(Boss boss, Hero[] heroes) {
+
+    }
+}
